9c78c97ad94008c271be7af3cfe6fdb7dbd72152
Th4Dude <gallottimaurizio@yahoo.it>
2023-03-06 18:38:01 +0200

    numeri scompaiono dopo tot secondi

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 'use strict'
 
+/* Funzione generatore random numeri */
+
 function getRndInteger(max) {
     return Math.floor(Math.random() * max) + 1;    
   }
   
-  const numeriDom = document.getElementById('demo1')
+  function NumeriDaNascondere(){
+    numeriDom.innerHTML = '';
+}
+
+ /* Funzione generatore random numeri */ 
+
+ /* array di numeri */
+
+ const numeriDom = document.getElementById('demo');
 
   for(let i = 0 ; i < 5; i++){
     let numeriRandom = getRndInteger(100);
-    console.log(numeriRandom)
-    numeriDom.innerHTML += numeriRandom;
+    console.log(numeriRandom);
+    numeriDom.innerHTML += numeriRandom + ' ';
 }
 
- 
+/* array di numeri */
+
+/* Timer con funzione */
+
+setTimeout( NumeriDaNascondere, 5 * 1000);
+
+/* Timer con funzione */
+
+const myTimeout = setTimeout( prompt('inserisci un numero'), 10 * 1000);
